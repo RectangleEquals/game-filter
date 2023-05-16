9a517714c1c5a943ea688424d50f93171d17539e
Michael Wion <rectangleequals@gmail.com>
2023-05-16 08:05:43 -0700

    added missing import

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const corsWhitelist = require("./src/corsWhitelist");
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const { getPassport, passport } = require('./src/passport');
+const path = require('path');
 //const discordStrategy = require('./src/strategies/discord');
 const localStrategy = require('./src/strategies/local');
 //const routes = require("./src/routes");

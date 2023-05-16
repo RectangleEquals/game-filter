5e654ca9b87c261046c048cff5f807e90af482db
Michael Wion <rectangleequals@gmail.com>
2023-05-16 08:00:02 -0700

    Fixed env for production

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
-if (process.env.NODE_ENV !== "production")
-  require("dotenv").config();
+// if (process.env.NODE_ENV !== "production")
+//   require("dotenv").config();
 
 /*
 // Assign some defaults to any potentially unassigned env variables

532fa25c6a9f55065e83dfb9c10bb298060edd31
Michael Wion <rectangleequals@gmail.com>
2023-05-16 12:39:29 -0700

    New approach

--- server.js
+++ server.js
@@ -12,39 +12,40 @@ const path = require('path');
 //const discordStrategy = require('./src/strategies/discord');
 const localStrategy = require('./src/strategies/local');
 //const routes = require("./src/routes");
-const { spawnSync  } = require('child_process');
+//const { spawnSync  } = require('child_process');
 
 const oneDayInMilliseconds = 86400000;
 
-function execute(cwd, command, exitOnFail = false) {
-  try {
-    console.log(`Executing command '${command}' in path '${cwd}'...`);
-    const output = spawnSync(command.split(' ')[0], command.split(' ').slice(1), { cwd, stdio: 'inherit' });
+// function execute(cwd, command, exitOnFail = false) {
+//   try {
+//     console.log(`Executing command '${command}' in path '${cwd}'...`);
+//     const output = spawnSync(command.split(' ')[0], command.split(' ').slice(1), { cwd, stdio: 'inherit' });
 
-    if (output.status === 0) {
-      console.log(`[Output ('${command}')]: ${output.stdout}`);
-      return true;
-    } else {
-      console.error(`[Output ('${command}' error): ${output.error}`);
-      console.log(`[Output ('${command}') stderror]: ${output.stderr}`);
-      if(exitOnFail)
-        process.exit(1);
-    }
-  } catch (error) {
-    console.error(`[Command ('${command}' error): ${error.message}`);
-  }
+//     if (output.status === 0) {
+//       console.log(`[Output ('${command}')]: ${output.stdout}`);
+//       return true;
+//     } else {
+//       console.error(`[Output ('${command}' error): ${output.error}`);
+//       console.log(`[Output ('${command}') stderror]: ${output.stderr}`);
+//       if(exitOnFail)
+//         process.exit(1);
+//     }
+//   } catch (error) {
+//     console.error(`[Command ('${command}' error): ${error.message}`);
+//   }
 
-  return false;
-}
+//   return false;
+// }
 
 async function run()
 {
   console.log('Starting server...');
 
   // Build the client
-  console.log('Building client...');
-  const rootDir = process.cwd();
-  execute(path.join(rootDir, 'client'), `${rootDir}/npx vite build`, true);
+  // console.log('Building client...');
+  // const rootDir = process.cwd();
+  // const npxPath = path.join(__dirname, 'node_modules', '.bin', 'npx');
+  // execute(path.join(rootDir, 'client'), `${rootDir}/npx vite build`, true);
 
   // Connect to remote database and init server
   try {
